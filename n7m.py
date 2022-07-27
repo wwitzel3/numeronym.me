f0ba7a01c61257dd056bdfffac9c05eb7dfcede7
Adam Emery <aqemery@gmail.com>
2022-07-27 14:43:29 -0400

    Changed input and button to pyscript built-in elements

--- n7m.py
+++ n7m.py
@@ -1,13 +1,8 @@
-from pyodide import create_proxy
-
-def generate_numeronym(document):
-    def _gn(*args, **kwargs):
-        input_value = document.getElementById("user-input").value
-        first = input_value[0]
-        last = input_value[-1]
-        num = len(input_value[1:-1])
-        output_value = f"{first}{num}{last}"
-        document.getElementById("output-target").innerText  = output_value
-    return create_proxy(_gn)
+def generate_numeronym(document, input_value):
+    first = input_value[0]
+    last = input_value[-1]
+    num = len(input_value[1:-1])
+    output_value = f"{first}{num}{last}"
+    document.getElementById("output-target").innerText = output_value
 
 __all__ = ['generate_numeronym']
